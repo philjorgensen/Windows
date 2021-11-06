799ee9a48135a14af93b4ec1d0a08be9d2b66c94
Philip Jorgensen <pjorgensen@lenovo.com>
2021-11-06 15:18:45 -0400

    Create Add-DMAComponentsToWhitelist.ps1

---
+++ Add-DMAComponentsToWhitelist.ps1
@@ -0,0 +1,20 @@
+﻿<#
+Adds un-allowed DMA component to whitelist to resolve automatic device encryption issues.
+Only necessary for Whiskey Lake generation ThinkPad
+
+Reference: https://docs.microsoft.com/en-us/windows-hardware/design/device-experiences/oem-bitlocker#un-allowed-dma-capable-busdevices-detected
+#>
+$Path = "SYSTEM\CurrentControlSet\Control\DmaSecurity\AllowedBuses"
+$Keys = @{
+
+    'PCI Express Upstream Switch Port' = 'PCI\VEN_8086&DEV_15C0'
+
+}
+
+if (!(Get-PSDrive HKLM -ErrorAction SilentlyContinue)) {
+    New-PSDrive -Name HKLM -PSProvider Registry -Root Registry::HKEY_LOCAL_MACHINE | Out-Null
+}
+
+foreach ($Key in $Keys.GetEnumerator()) {
+    New-ItemProperty -Path HKLM:$Path -Name $Key.Key -Value $Key.Value -PropertyType String -Force | Out-Null
+}
